ba92c7d5498bdbc708d13ebf654f157b7d57ddc4
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-01-16 15:19:34 +0330

    modify test_saliency

--- saliency_prediction/test_saliency_prediction.py
+++ saliency_prediction/test_saliency_prediction.py
@@ -5,37 +5,58 @@ from saliency_prediction.utils.data_process import preprocess_img, postprocess_i
 from saliency_prediction.model import ECT_SAL
 
 
-def saliency_map_prediction(img_path , text_map_path , weight_path ):
+def saliency_map_prediction(img_path, text_map_path, weight_path):
+    """
+    Predicts the saliency map of an image given its path and a text map path using the ECT_SAL model.
 
-    # Set Device
+    Parameters:
+    img_path (str): Path to the image file.
+    text_map_path (str): Path to the text map file.
+    weight_path (str): Path to the weights file for the ECT_SAL model.
+
+    Returns:
+    PIL.Image: A PIL Image object of the predicted saliency map.
+
+    This function loads a pre-trained ECT_SAL model, processes the input image and text map,
+    and predicts the saliency map. The result is post-processed and returned as a PIL image.
+    """
+
+    # Set the computation device (GPU if available, else CPU)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    # load Model
+    # Load the ECT_SAL model
     model = ECT_SAL()
     model.load_state_dict(torch.load(weight_path), strict=False)
     model = model.to(device)
     model.eval()
     print("Model loaded...")
 
+    # Preprocess the image and text map
     img = preprocess_img(img_path)
-    name = img_path.split('/')[-1].split('.')[0]
     tmap = preprocess_img(text_map_path)
 
+    # Normalize and reshape the image and text map for the model
     img = np.array(img) / 255.
     tmap = np.array(tmap) / 255.
-
     img = np.expand_dims(np.transpose(img, (2, 0, 1)), axis=0)
     tmap = np.expand_dims(np.transpose(tmap, (2, 0, 1)), axis=0)
 
+    # Convert to torch tensors
     img = torch.from_numpy(img)
     tmap = torch.from_numpy(tmap)
 
+    # Move tensors to the computation device
     img = img.type(torch.cuda.FloatTensor).to(device)
     tmap = tmap.type(torch.cuda.FloatTensor).to(device)
 
+    # Predict the saliency map
     pred_saliency = model(img, tmap)
+
+    # Convert the prediction to a PIL Image
     toPIL = transforms.ToPILImage()
     pic = toPIL(pred_saliency.squeeze())
+
+    # Postprocess the saliency map
     pred_saliency = postprocess_img(pic, img_path)
 
     return pred_saliency
