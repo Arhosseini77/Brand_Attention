7e9bc03b8eaf544be41d2ef730c54eedf2d3b8c4
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-01-17 10:24:18 +0330

    modify main object attention

--- main_object_attention.py
+++ main_object_attention.py
@@ -1,8 +1,8 @@
 """
- Interface for Brand-Attention Score Calculation.
+ Interface for any Object-Attention Score Calculation.
 
  Usage:
- python main_brand_attention.py --img_path path/to/input_image.jpg --tmap path/to/text_map.jpg
+ python main_object_attention.py --img_path path/to/input_image.jpg --tmap path/to/text_map.jpg
 
  Arguments:
  --img_path : Path to the input image file.
@@ -15,7 +15,7 @@ from brand_attention_module.brand_attention_module import object_attention_calc
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Brand Change Score Calculation')
+    parser = argparse.ArgumentParser(description='object attention Score Calculation')
     parser.add_argument('--img_path', type=str, default='test_images/test.jpg', help='path to input image')
     parser.add_argument('--tmap', type=str, default='test_images/test_tmap.jpg', help='path to text map image')
     args = parser.parse_args()
@@ -23,7 +23,7 @@ def main():
     # Calculate brand change score
     brand_change_score = object_attention_calc(args.img_path, args.tmap)
 
-    print(f"Brand Change Score: {brand_change_score}")
+    print(f"Object-Attention Score: {brand_change_score}")
 
 
 if __name__ == '__main__':
